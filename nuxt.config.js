7d6a6919185e79ca019311b282888c6aaa6d45ed
Damien Robinson <damien.robinson@pocketpasta.com>
2021-05-31 09:27:10 +1000

    chore(config): add title template

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,88 @@
-import colors from 'vuetify/es5/util/colors'
+import pkg from './package'
+
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || '3000'
+const BASE_URL = (
+  process.env.BASE_URL ||
+  process.env.DEPLOY_URL ||
+  process.env.URL ||
+  process.env.VERCEL_URL ||
+  `http${PORT === 433 ? 's' : ''}://${HOST}${
+    [433, 80].includes(PORT) ? '' : `:${PORT}`
+  }`
+).replace(/(^http[s]?)?(?::\/\/)?(.*)/, function (_, protocol, domain) {
+  return `${protocol || 'http'}://${domain}`
+})
+
+const env = {
+  HOST,
+  PORT,
+  BASE_URL,
+  VERSION: pkg.version,
+  COMMIT:
+    process.env.npm_package_gitHead || process.env.VERCEL_GITHUB_COMMIT_SHA,
+  DATE_GENERATED: new Date().toISOString(),
+  APP_NAME:
+    process.env.APP_NAME ||
+    `${pkg.name.charAt(0).toUpperCase()}${pkg.name.slice(1)}`,
+}
+
+const preconnectLinks = []
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  generate: {
+    // use '404.html' instead default '200.html'
+    fallback: true,
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - gondwanarama',
-    title: 'gondwanarama',
+    title: env.APP_NAME,
+    titleTemplate: `%s | ${env.APP_NAME}`,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        once: true,
+        name: 'charset',
+        hid: 'charset',
+        content: 'utf-8',
+      },
+      {
+        once: true,
+        hid: 'viewport',
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        once: true,
+        name: 'version',
+        hid: 'version',
+        content: env.VERSION,
+      },
+      {
+        once: true,
+        property: 'og:title',
+        content: env.APP_NAME,
+        template: `%s | ${env.APP_NAME}`,
+        hid: 'og:title',
+      },
+      {
+        name: 'version',
+        content: pkg.version,
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ...preconnectLinks.map((href) => ({
+        rel: 'preconnect',
+        href,
+        hid: `preconnect-${href}`,
+        crossorigin: 'anonymous',
+        once: true,
+      })),
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
