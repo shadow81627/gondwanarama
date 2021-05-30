7b867bc30be124e020df738921db00c4851ce75b
Damien Robinson <damien.robinson@pocketpasta.com>
2021-05-31 09:50:10 +1000

    chore(error): 404 and 500 pages

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,39 +1,37 @@
 <template>
-  <v-container class="nuxt-error pa-0 fill-height" fluid>
-    <v-row no-gutters class="pa-0 align-self-center">
-      <v-col class="pa-0" align="center">
-        <h1 v-if="error.statusCode === 404">
-          {{ pageNotFound }}
-        </h1>
-        <h1 v-else>
-          {{ otherError }}
-        </h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app dark>
+    <v-container class="nuxt-error pa-0 fill-height" fluid>
+      <v-row no-gutters class="pa-0 align-self-center">
+        <v-col class="pa-0" align="center">
+          <div class="nuxt-error container">
+            <component :is="errorPage" :error="error" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import error404 from '@/components/error/404.vue'
+import error500 from '@/components/error/500.vue'
+
 export default {
   layout: 'empty',
   props: {
     error: {
       type: Object,
-      default: null,
+      default: () => {},
     },
   },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
+  computed: {
+    errorPage() {
+      if (this.error.statusCode === 404) {
+        return error404
+      }
+      // catch everything else
+      return error500
+    },
   },
 }
 </script>
