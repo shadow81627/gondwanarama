7898126f1a4224e7ef24eb1d798b1a88c88be216
Damien Robinson <damien.robinson@daim.dev>
2021-05-31 15:17:57 +1000

    fix(color): darker link color for accessable contrast

--- vuetify.options.js
+++ vuetify.options.js
@@ -2,4 +2,15 @@ export default {
   icons: {
     iconfont: 'mdiSvg',
   },
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: '#1a1a1a',
+      },
+      dark: {
+        primary: '#1a1a1a',
+      },
+    },
+  },
 }
