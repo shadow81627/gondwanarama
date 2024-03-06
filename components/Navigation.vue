ddb06c3575c52595f8d78498f3b69d8492723df2
Damien Robinson <damien.robinson@daim.dev>
2024-03-06 12:13:26 +1000

    fix(navigation component): wrap links and mobile button to prevent movement on load

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,6 @@
 <template>
   <nav
-    class="relative w-full flex flex-wrap items-center justify-between text-lg"
-    bg-nav
+    class="relative w-full flex flex-wrap items-center justify-between text-lg bg-nav"
   >
     <div
       class="container-fluid w-full flex flex-wrap items-center justify-between px-6 h-109px"
@@ -15,54 +14,56 @@
         </NuxtLink>
       </div>
 
-      <div class="hidden lg:flex justify-between">
-        <NuxtLink
-          v-for="item of items"
-          :key="item.name"
-          :to="item.url"
-          class="hover:text-gray-800 text-black-500 text-xl btn"
-          :class="{
-            'bg-gray-200': $route.path === item.url,
-            'hover:bg-gray-500/10': $route.path !== item.url,
-          }"
-          itemscope
-          itemtype="https://schema.org/SiteNavigationElement"
-          :target="item.url.startsWith('http') ? '_blank' : undefined"
+      <div>
+        <div class="hidden lg:flex justify-between">
+          <NuxtLink
+            v-for="item of items"
+            :key="item.name"
+            :to="item.url"
+            class="hover:text-gray-800 text-black-500 text-xl btn"
+            :class="{
+              'bg-gray-200': $route.path === item.url,
+              'hover:bg-gray-500/10': $route.path !== item.url,
+            }"
+            itemscope
+            itemtype="https://schema.org/SiteNavigationElement"
+            :target="item.url.startsWith('http') ? '_blank' : undefined"
+          >
+            {{ item.name }}
+          </NuxtLink>
+        </div>
+
+        <ClientOnly>
+          <Teleport to="#sidenav">
+            <NavigationDraw v-model:draw="draw" :items="items"></NavigationDraw>
+          </Teleport>
+        </ClientOnly>
+
+        <button
+          class="navbar-toggler lg:hidden text-black border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
+          type="button"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          @click="draw = !draw"
         >
-          {{ item.name }}
-        </NuxtLink>
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="bars"
+            class="w-6"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              fill="currentColor"
+              d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+            ></path>
+          </svg>
+        </button>
       </div>
-
-      <ClientOnly>
-        <Teleport to="#sidenav">
-          <NavigationDraw v-model:draw="draw" :items="items"></NavigationDraw>
-        </Teleport>
-      </ClientOnly>
-
-      <button
-        class="navbar-toggler lg:hidden text-black border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
-        type="button"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        @click="draw = !draw"
-      >
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="bars"
-          class="w-6"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-        >
-          <path
-            fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-          ></path>
-        </svg>
-      </button>
     </div>
   </nav>
 </template>
